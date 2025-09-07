ba6e22b7b12f3ca5d1d74521e526bbd302ad02cb
jay personal <thottempudi56@gmail.com>
2025-09-07 21:47:58 +0530

    resolved mobile view issues

--- src/body/about/about.js
+++ src/body/about/about.js
@@ -7,22 +7,23 @@ import { faArrowAltCircleDown } from '@fortawesome/free-solid-svg-icons'
 function about(){
     return(
         <div className=" container-fluid about px-0" id="aboutID">
-            {/* <div className="background px-3"> */}
                 <div className="col-lg-8 col-md-12 col-sm-12 content">
                     <h1>Hey there,<br/> I'm Jaya Chandra Thottempudi</h1><br/>
                     <p>Results-driven Software Developer with 6+ years of experience in building scalable, high-performance web and mobile applications. Strong expertise in React.js, Node.js, and PHP for full-stack development, along with hands-on experience in developing native Android applications using Java.
                     <br /> For the past year, actively working as a Guidewire Jutro Digital Platform (JDP) Developer, developing and customizing Digital Applications integrated with Guidewire PolicyCenter, ClaimCenter, and BillingCenter using the Jutro framework and Guidewire Edge APIs.</p>
                     <br/><br/>
+                    
+                <div className="about">
                     <span className="btn-outline-primary btn btn-color"><a href="https://github.com/jayachandra56?tab=repositories" target="_blank">GitHub Profile</a></span>
-                    &nbsp;&nbsp;
+                    <span className="btn-outline-primary btn btn-color"><a href="https://www.linkedin.com/in/thottempudi7056/" target="_blank">LinkedIn Profile</a></span>
                     <span className="btn-outline-primary btn btn-color">
                         <a href={resume} target="_blank" rel="noopener noreferrer">Download Resume</a>
                     </span>
                 </div>
+                </div>
                 <div className="col-lg-4 col-md-12 col-sm-12">
                     <img src={web} className="imageAbt" alt="Web Development"/><br/>    
                 </div> 
-            {/* </div> */}
         </div>
 
     )
